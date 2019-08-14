0071d1fc6563a4f513dfc5444461df3c0f3029f2
David Latorre <david@latorredev.com>
2019-08-14 00:06:04 +0000

    [Update] Update script deleting quotes and adding flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 --- create database, new user, set pasword and permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO hbnb_dev@localhost;
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema . * TO hbnb_dev@localhost;
