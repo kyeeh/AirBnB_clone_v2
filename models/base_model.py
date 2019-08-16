49e8b739fe7eef81fdac1eba5dd43e4bd1190b18
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-08-16 01:37:07 +0000

    Adding changes to base model

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
@@ -65,4 +66,12 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if "_sa_instance_state" in my_dict.keys():
+            del(my_dict["_sa_instance_state"])
         return my_dict
+
+    def delete(self):
+        """ 
+        Deletes the current instance from the storage
+        """
+        models.storage.delete(self)
