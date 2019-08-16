bcbc4be769a670ae7a9aa027c9857c95819b6eaf
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-08-16 03:32:53 +0000

    Adding requirements for state model

--- models/state.py
+++ models/state.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from os import getenv
+from models import storage
+from models.city import City
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship(City, backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            '''
+            City list for FileStorage
+            '''
+            cities_by_state = []
+            for city in storage.all('City').values():
+                if city.state_id == self.id:
+                    cities_by_state.append(city)
+            return cities_by_state
